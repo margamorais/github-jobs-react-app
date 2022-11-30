58d2d100d2e84043da8819f5edefaef03f37a662
margamorais <margamorais@deloitte.pt>
2022-11-30 11:11:47 +0000

    test application

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const axios = require('axios');
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
 const app = express();
 
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
-app.use(cors());
+// app.use(cors());
 
 app.get('/jobs', async (req, res) => {
   try {
